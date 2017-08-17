4ae66ac417b7e713312fa1685d08ee149c2368a8
300932732 <300932732@PR-A315-33>
2017-08-17 11:38:53 -0400

    Added fields and properties to class ScoreBoard

---
+++ COMP123-S2017-FinalExam-StudentID/ScoreBoard.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2017_FinalExam_StudentID
+{/// <summary>
+/// This class is called ScoreBoard.
+/// </summary>
+    class ScoreBoard
+    {
+        // PRIVATE FIELDS (Instance Variables)----------------------
+        TextBox _finalScoreTextBox;
+        int _score;
+        TextBox _scoreTextBox;
+        int _time;
+        TextBox _timeTextBox;
+
+        // PUBLIC PROPERTIES ----------------------------------------
+        /// <summary>
+        /// These are the five public properties of this class
+        /// </summary>
+        public TextBox FinalScoreTextBox
+        {
+            get
+            {
+                return this._finalScoreTextBox;
+            }
+            set
+            {
+                this._finalScoreTextBox = value;
+            }
+
+        }
+        public int Score
+        {
+            get
+            {
+                return this._score;
+            }
+            set
+            {
+                this._score = value;
+                
+            }
+        }
+        public TextBox ScoreTextBox
+        {
+            get
+            {
+                return this._scoreTextBox;
+            }
+            set
+            {
+                this._scoreTextBox = value;
+            }
+        }
+        public int Time
+        {
+            get
+            {
+                return this._time;
+            }
+            set
+            {
+                this._time = value;
+            }
+        }
+        public TextBox TimeTextBox
+        {
+            get
+            {
+                return this._timeTextBox;
+            }
+            set
+            {
+                this._timeTextBox=value;
+            }
+        }
+
+    }
+}
